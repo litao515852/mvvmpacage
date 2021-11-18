60e5dc206f1d5ed6bbc64f6e1396963ecc53b55c
LiTao602 <tao.li602@cdzkhy.com>
2021-11-18 19:37:15 +0800

    以一次提交

---
+++ app/src/main/java/com/lzkhy/moble/zkhywater/di/NetworkModule.kt
@@ -0,0 +1,73 @@
+package com.lzkhy.moble.zkhywater.di
+
+
+/*
+ * Designed and developed by 2020 skydoves (Jaewoong Eum)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import com.lzkhy.moble.zkhywater.HttpClient
+import com.lzkhy.moble.zkhywater.HttpRequestInterceptor
+import com.lzkhy.moble.zkhywater.HttpServices
+import com.skydoves.sandwich.coroutines.CoroutinesResponseCallAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+/**
+ * Created by lit
+ * on 2021/11/17.
+ * for  通过hilt注入此类
+ */
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(HttpRequestInterceptor())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .client(okHttpClient)
+            .baseUrl("https://pokeapi.co/api/v2/")
+            .addConverterFactory(MoshiConverterFactory.create())
+            .addCallAdapterFactory(CoroutinesResponseCallAdapterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun providePokedexService(retrofit: Retrofit): HttpServices {
+        return retrofit.create(HttpServices::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun providePokedexClient(httpServices: HttpServices): HttpClient {
+        return HttpClient(httpServices)
+    }
+}
---
+++ app/src/main/java/com/lzkhy/moble/zkhywater/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.lzkhy.moble.zkhywater.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.lzkhy.moble.zkhywater.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
